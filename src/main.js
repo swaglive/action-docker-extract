7749e80bfcdf3ea2ab7df4cb809c1fee758ffc91
Allan Lei <allanlei@helveticode.com>
2024-01-08 01:19:03 +0800

    Add action

---
+++ src/main.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const io = require('@actions/io')
+
+module.exports = async () => {
+  const image = core.getInput('image')
+  const sources = core.getMultilineInput('sources')
+  const destination = core.getInput('destination')
+  const cleanup = core.getBooleanInput('cleanup')
+
+  // Generate options for `docker cp`
+  const optionsForCP = []
+  if (core.getBooleanInput('follow-link')) options.push('--follow-link')
+
+  // Create container
+  const { stdout: containerId } = await exec.getExecOutput(
+    'docker', ['create', image],
+  )
+  let { stdout: container } = await exec.getExecOutput(
+    'docker', ['inspect', containerId.trim()],
+  )
+  [ container ] = JSON.parse(container)
+
+  // Create `destination` directory
+  await io.mkdirP(destination)
+
+  // Copy files from container to `destination`
+  await Promise.all(sources.map(
+    source => exec.exec('docker', ['cp', ...optionsForCP, `${container.Id}:${path.resolve(container.Config.WorkingDir, source)}`, destination])
+  ))
+
+  core.setOutput('destination', destination)
+
+  if (cleanup) {
+    // Remove container
+    await exec.exec('docker', ['rm', container.Id])
+  } else {
+    core.setOutput('container', container)
+  }
+}
